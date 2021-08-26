a1d92da90b8a850f3ff690aff4051c66f1b0fbd5
GayFrog <89565904+GayFrog@users.noreply.github.com>
2021-08-25 23:50:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ function formatAndSendTweet(event) {
 
     console.log(tweetText);
 
-    return tweet.handleDupesAndTweet(tokenName, tweetText, image);
 }
 
 // Poll OpenSea every minute & retrieve all sales for a given collection in the last minute

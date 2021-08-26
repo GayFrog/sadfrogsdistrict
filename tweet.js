47bc25a75ce5d318d9b76eba30eb4e5bf0dd251d
GayFrog <89565904+GayFrog@users.noreply.github.com>
2021-08-25 23:49:04 -0700

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -12,37 +12,6 @@ const twitterConfig = {
 
 const twitterClient = new twit(twitterConfig);
 
-// OpenSea doesn't give us access to Webhooks; need to poll every 60 seconds
-// Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
-async function handleDupesAndTweet(tokenName, tweetText, imageUrl) {
-    // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
-    twitterClient.get('search/tweets', { q: tokenName, count: 1, result_type: 'recent' }, (error, data, response) => {
-        if (!error) {
-            const statuses = _.get(data, 'statuses');
-
-            // No duplicate statuses found
-            if (_.isEmpty(data) || _.isEmpty(statuses)) {
-                console.log('No duplicate statuses found, continuing to tweet...');
-
-                return tweet(tweetText, imageUrl);
-            }
-
-            const mostRecentMatchingTweetCreatedAt = _.get(statuses[0], 'created_at');
-            const statusOlderThan10Mins = moment(mostRecentMatchingTweetCreatedAt).isBefore(moment().subtract(10, 'minutes'));
-
-            // Status found is older than 10 minutes, not a cached transaction, just sold at same price
-            if (statusOlderThan10Mins) {
-                console.log('Previous status is older than 10 minutes, continuing to tweet...');
-
-                return tweet(tweetText, imageUrl);
-            }
-
-            console.error('Tweet is a duplicate; possible delayed transaction retrieved from OpenSea');
-        } else {
-            console.error(err);
-        }
-    });
-}
 
 // Upload image of item retrieved from OpenSea & then tweet that image + provided text
 async function tweet(tweetText, imageUrl) {
